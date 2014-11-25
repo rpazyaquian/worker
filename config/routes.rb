b6e59680057bc955d169b2caddc467b412b29803
Rebecca Paz <r.pazyaquian@gmail.com>
2014-11-25 15:10:53 -0500

    added sync and async main routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  get 'sync' => 'sync#index'
+  get 'async' => 'async#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
